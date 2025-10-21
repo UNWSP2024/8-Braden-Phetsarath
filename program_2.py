8ae106c5406d30df503b118c622615694a50b28f
Braden-Phetsarath <bkphetsarath@students.unwsp.edu>
2025-10-21 10:27:41 -0500

    Update program_2.py

--- program_2.py
+++ program_2.py
@@ -1,8 +1,11 @@
+#Braden Phetsarath
+#10/20
+
 # Program #2: Word Separator
-# Write a program that accepts as input a sentence in which all of the words are run together, 
-# but the first character of each word is uppercase.  
-# Convert the sentence to a string in which the words are separated by spaces, 
-# and the first word starts with an uppercase.  
+# Write a program that accepts as input a sentence in which all of the words are run together,
+# but the first character of each word is uppercase.
+# Convert the sentence to a string in which the words are separated by spaces,
+# and the first word starts with an uppercase.
 # For example the string "StopAndSmellTheRoses" would be converted to "Stop and smell the roses."
 
 # Start your changes on line 13
@@ -11,8 +14,18 @@ def word_separator(sentence):
 
     new_sentence = ""
     #    Add your logic here
+    new_sentence = new_sentence + sentence[0]
 
-    return new_sentence.strip()
+    for i in range(1 , len(sentence)):
+        letter = sentence[i]
+
+        if letter.isupper():
+            letter = letter.lower()
+            new_sentence = new_sentence + ' '
+
+        new_sentence = new_sentence + letter
+
+    return new_sentence.strip() + '.'
 
 # Example usage
 
